2124c969d5892d50439ff56b169e02e8f73a3cae
Akshat Goyal <46156730+DanteAgarwal@users.noreply.github.com>
2023-10-11 21:52:44 +0530

    cleaning and strautcting again

--- js/main.js
+++ js/main.js
@@ -4,38 +4,38 @@
 
 
     // Dropdown on mouse hover
-    // $(document).ready(function () {
-    //     function toggleNavbarMethod() {
-    //         if ($(window).width() > 992) {
-    //             $('.navbar .dropdown').on('mouseover', function () {
-    //                 $('.dropdown-toggle', this).trigger('click');
-    //             }).on('mouseout', function () {
-    //                 $('.dropdown-toggle', this).trigger('click').blur();
-    //             });
-    //         } else {
-    //             $('.navbar .dropdown').off('mouseover').off('mouseout');
-    //         }
-    //     }
-    //     toggleNavbarMethod();
-    //     $(window).resize(toggleNavbarMethod);
-    // });
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
 
-    // $(document).ready(function () {
-    //     function toggleDropdown() {
-    //         if ($(window).width() > 992) {
-    //             $('.nav-item.dropdown').on('mouseenter', function () {
-    //                 $(this).find('.dropdown-menu').addClass('show');
-    //             }).on('mouseleave', function () {
-    //                 $(this).find('.dropdown-menu').removeClass('show');
-    //             });
-    //         } else {
-    //             $('.nav-item.dropdown').off('mouseenter').off('mouseleave');
-    //         }
-    //     }
+    $(document).ready(function () {
+        function toggleDropdown() {
+            if ($(window).width() > 992) {
+                $('.nav-item.dropdown').on('mouseenter', function () {
+                    $(this).find('.dropdown-menu').addClass('show');
+                }).on('mouseleave', function () {
+                    $(this).find('.dropdown-menu').removeClass('show');
+                });
+            } else {
+                $('.nav-item.dropdown').off('mouseenter').off('mouseleave');
+            }
+        }
 
-    //     toggleDropdown(); // Initial setup
-    //     $(window).resize(toggleDropdown); // Update on window resize
-    // });
+        toggleDropdown(); // Initial setup
+        $(window).resize(toggleDropdown); // Update on window resize
+    });
 
     // Back to top button
     $(window).scroll(function () {
